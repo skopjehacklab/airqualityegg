f9d80f155234be44330a0771286c58231cfb0012
Aleks <aleksandar.lazarov1@gmail.com>
2013-02-13 12:48:46 +0100

    first code commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Code for the air quality eggs that we received from the techinc hackerspace[1] from Amsterdam
+
+
+[1] http://techinc.nl/
---
+++ config.ino
@@ -0,0 +1,5 @@
+/* antw004feeder live
+*/
+byte mymac[] = { 0x50,0x90,0x00,0x48,0x24,0x00 };
+#define FEED    "102108"
+#define APIKEY  "HPnvmL3EdJDavnr-Xu-DPiWEc8mSAKxESVhYOXZUY1JYRT0g"
